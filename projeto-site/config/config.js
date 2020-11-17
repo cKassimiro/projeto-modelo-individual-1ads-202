86f7f1a7859f43518319e9e19779c3eb205a2599
Fernanda Caramico <46379117+fernandacaramico@users.noreply.github.com>
2020-11-17 17:01:02 -0300

    Update config.js

--- projeto-site/config/config.js
+++ projeto-site/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   production: {
-    username: 'grupoteste',
-    password: '#Gfgrupot',
-    database: '202-grupot',
-    host: '202-grupot.database.windows.net',
+    username: 'SEU_USUARIO_DO_BANCO_DE_DADOS',
+    password: 'SUA_SENHA_DO_BANCO_DE_DADOS',
+    database: 'NOME_DO_SEU_BANCO_DE_DADOS',
+    host: 'URL_DO_SEU_BANCO_DE_DADOS.database.windows.net',
     dialect: 'mssql',
     xuse_env_variable: 'DATABASE_URL',
     dialectOptions: {
